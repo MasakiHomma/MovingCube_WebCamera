7750b43d8da26628b7437eb6338c84f891fa8f49
Masaki Homma <masakihomma991201@gmail.com>
2017-02-17 20:48:34 +0900

    First Commit

---
+++ Assets/CubeScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UniRx;
+using UniRx.Triggers;
+using System.Linq;
+using System.Collections.Generic;
+
+public class CubeScript : MonoBehaviour {
+
+	float x_Position = -11;
+	float z_Position = -11;
+
+	public List<GameObject> gameObjeList = new List <GameObject>();
+
+	void Start () {
+
+		while (x_Position <= 12.1f) {
+			while (z_Position <= 12.1f) {
+				var cube = GameObject.CreatePrimitive (PrimitiveType.Cube);
+				cube.transform.position = new Vector3 (x_Position, 0, z_Position);
+				gameObjeList.Add (cube);
+
+				z_Position += 1.1f;
+			}
+			x_Position += 1.1f;
+			z_Position = -11;
+		}
+
+		Debug.Log (""+gameObjeList.Count);
+
+//		this.UpdateAsObservable ()
+//			.SkipUntil (__ => {
+//				
+//			})
+//			.Subscribe (_ => {
+//
+//		});
+
+//		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+//		RaycastHit hit = new RaycastHit ();
+		    
+	}
+}
---
+++ Assets/WebCamScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Linq;
+
+public class WebCamScript : MonoBehaviour {
+	public int Width = 1920;
+	public int Height = 1080;
+	public int FPS = 30;//1秒間に何回WebCameraの画像を取得するかの変数
+
+	public WebCamTexture webcamTexture;
+	public Color[] pixelsDate;
+
+	CubeScript cubeScript;
+
+	void Start () {
+		cubeScript = gameObject.GetComponent <CubeScript> ();
+
+		WebCamDevice[] devices = WebCamTexture.devices;
+		webcamTexture = new WebCamTexture(devices[0].name, Width, Height, FPS );
+		webcamTexture.Play();
+	}
+
+	void Update () {
+		for (int i = 0;i <= Mathf.Sqrt(cubeScript.gameObjeList.Count); i++) {
+			for (int j = 0; j <= Mathf.Sqrt (cubeScript.gameObjeList.Count); j++) {
+				pixelsDate = webcamTexture.GetPixels (	(int)(webcamTexture.width/
+														Mathf.Sqrt (cubeScript.gameObjeList.Count)*i),
+														(int)(webcamTexture.height/
+														Mathf.Sqrt (cubeScript.gameObjeList.Count)*j),
+														(int)(webcamTexture.width/Mathf.Sqrt (cubeScript.gameObjeList.Count)/1.1f),
+														(int)(webcamTexture.width/Mathf.Sqrt (cubeScript.gameObjeList.Count)/1.1f) );
+				Texture2D texture = new Texture2D(	(int)( webcamTexture.width/Mathf.Sqrt (cubeScript.gameObjeList.Count)/1.1f ),
+													(int)( webcamTexture.height/Mathf.Sqrt (cubeScript.gameObjeList.Count)/1.1f )  );
+
+				texture.SetPixels(pixelsDate);
+				texture.Apply();
+
+				cubeScript.gameObjeList[(  i*(int)( Mathf.Sqrt (cubeScript.gameObjeList.Count) )  )
+										+j].GetComponent<Renderer> ().material.mainTexture = texture;
+			}
+		}
+	}
+}
